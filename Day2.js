6dc943bf62639100f66a9fabc0d55dfa9a4533be
akshata1111 <akshata.paltiwale.ap@gmail.com>
2024-04-11 12:27:45 +0530

    first commit

---
+++ Day2.js
@@ -0,0 +1,45 @@
+//Day2 
+/*// External js
+console.log("Hi, I am External JS")
+
+//Day2html
+<!-- !   shortcut with that can create HTML5 basic structure -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- 1. inline js -->
+    <!-- can add js in HTML elemet -->
+
+    <!-- 2. Internal js -->
+    <script>
+        console.log("Hi, I am internal JS")
+    </script>
+
+    <!-- 3. External js -->
+    <script src="day2.js"></script>
+    <!-- src -> js file -->
+</body>
+</html>
+
+# Front end technologies- HTML,CSS, JS
+- HTML
+- CSS
+- Make dynamic web page - js help
+
+# DOM - Document Object Model
+- Modify the HTML structure, styling, content of web page, user input
+
+
+# Js scripts:- .js file
+
+# locations to put js code:-
+- with HTML
+1. inline js - onClick, onload
+2. internal js
+3. external js*/
